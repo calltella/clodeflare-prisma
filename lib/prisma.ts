7e74ff3ab1bb0f9264387083af29f49e414f5c6c
calltella <yuichi.asa@gmail.com>
2026-01-30 04:50:38 +0000

    notes add

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "../src/generated/prisma/client";
 import { PrismaD1 } from "@prisma/adapter-d1";
+import { PrismaClient } from "@prisma/client";
 
 export function createPrisma(db: D1Database) {
   return new PrismaClient({
